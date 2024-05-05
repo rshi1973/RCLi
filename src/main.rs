4711db8486b216fe0d0fa4fd1893b12fbc01f9f3
rshi1973 <rshi1973@hotmail.com>
2024-05-05 20:04:25 +0800

    Refactor: read in a csv and convert it into a json file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+//rcli csv -i input.csv -o output.json --header -d ','
+use clap::Parser;
+use anyhow::Result;
+use rcli::{Opts, SubCommand, process_csv};
+
+fn main() -> Result<()>{
+    let opts: Opts = Opts::parse();
+    
+    //match opts.cmd if it the command is csv, read the csv file and print the records
+    //and the error handling follows anyhow style using map_err
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            process_csv(&opts.input, &opts.output)?;
+        }
+    }
+    
+    Ok(())
 }
